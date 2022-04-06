1d82e50dd1e2f99783ad674f64816745c76cbe51
ThaliaLevy <thalialevy@hotmail.com>
2022-04-05 21:06:15 -0300

    inserindo txt anotacoes e classes da estrutura mvc

---
+++ src/control/Carro.java
@@ -0,0 +1,52 @@
+package control;
+
+public class Carro {
+
+	private String nome;
+	private String placa;
+	private String marca;
+	private int ano;
+	
+	public Carro() {
+		
+	}
+	
+	public Carro(String nome, String placa, String marca, int ano) {
+		this.nome = nome;
+		this.placa = placa;
+		this.marca = marca;
+		this.ano = ano;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+}
